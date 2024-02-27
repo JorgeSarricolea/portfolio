0b239fe9ca5dcf78637b07a98352b8fe96f0e273
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2024-02-27 17:18:15 -0600

    feat: Header component, HeaderMobile component

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#EAA0FD",
+        secondary: "#72507A",
+        gray: "#212121",
+      },
+    },
+  },
+  plugins: [],
+};
