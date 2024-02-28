9679d5fdbbb33e790cade00a85ac82b6fad32f6a
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2024-02-28 17:42:51 -0600

    feat: Delete typed effect in page title

--- src/js/Typed.js
+++ src/js/Typed.js
@@ -1,14 +1,6 @@
 import Typed from "typed.js";
 
 document.addEventListener("DOMContentLoaded", function () {
-  let pageTitle = new Typed("#pageTitle", {
-    strings: ["Fullstack Developer", "UX/UI Designer", "Jorge Sarricolea"],
-    loop: false,
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-  });
-
   let multiText = new Typed("#multiText", {
     strings: ["Fullstack Developer", "UX/UI Designer", "Jorge Sarricolea"],
     loop: true,
