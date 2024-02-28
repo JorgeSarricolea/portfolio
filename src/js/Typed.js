ec306005898ccef68512fb9856e6089010109ba6
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2024-02-27 18:04:08 -0600

    feat: Typed effect in page title

---
+++ src/js/Typed.js
@@ -0,0 +1,11 @@
+import Typed from "typed.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+  let pageTitle = new Typed("#pageTitle", {
+    strings: ["Fullstack Developer", "UX/UI Designer", "<JorgeSarricolea />"],
+    loop: false,
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+  });
+});
