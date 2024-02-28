09cb9a3002f8e8b368a9f13c28a2377f962977e8
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2024-02-27 18:27:12 -0600

    style: Update favicon

--- src/js/Typed.js
+++ src/js/Typed.js
@@ -2,7 +2,7 @@ import Typed from "typed.js";
 
 document.addEventListener("DOMContentLoaded", function () {
   let pageTitle = new Typed("#pageTitle", {
-    strings: ["Fullstack Developer", "UX/UI Designer", "<JorgeSarricolea />"],
+    strings: ["Fullstack Developer", "UX/UI Designer", "Jorge Sarricolea"],
     loop: false,
     typeSpeed: 100,
     backSpeed: 100,
