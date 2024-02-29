e0684cf0033c0137af500e72a8495613d2f81337
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2024-02-28 18:35:32 -0600

    fix: Responsiveness in Home component

--- src/js/Typed.js
+++ src/js/Typed.js
@@ -2,7 +2,7 @@ import Typed from "typed.js";
 
 document.addEventListener("DOMContentLoaded", function () {
   let multiText = new Typed("#multiText", {
-    strings: ["Fullstack Developer", "UX/UI Designer", "Jorge Sarricolea"],
+    strings: ["a Fullstack Developer", "a UX/UI Designer", "Jorge Sarricolea"],
     loop: true,
     typeSpeed: 30,
     backSpeed: 30,
