8b45f7ffe49649e752b61bce5241886024392d00
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2024-02-29 13:03:01 -0600

    Getting started

---
+++ src/js/projects.js
@@ -0,0 +1,21 @@
+const projects = [
+  {
+    id: 1,
+    title: "Datamatic Software",
+    description:
+      "I worked on different projects, designing the user interface from scratch, adding new features and reusable components for various projects.",
+    image: "../../public/img/DatamaticSoftware.jpg",
+    url: "https://www.datamaticsoftware.com",
+    tagIds: ["reactjs", "javascript", "astro"],
+  },
+  {
+    id: 2,
+    title: "Jumwork",
+    description:
+      "I designed the whole brand, UX/UI design from scratch on Bubble. Integration with Stripe API and others in backend process. Worked with different plugins, like Sendgrid and upgraded old pages to the new responsive engine.",
+    image: "../../public/img/JumworkTareas.png",
+    url: "https://www.jumwork.com",
+  },
+];
+
+export default projects;
---
+++ src/js/tags.js
@@ -0,0 +1,19 @@
+const tags = {
+  reactjs: {
+    id: 1,
+    name: "ReactJS",
+    image: "../../public/icons/reactjs-icon.png",
+  },
+  javascript: {
+    id: 2,
+    name: "JavaScript",
+    image: "../../public/icons/javascript-icon.png",
+  },
+  astro: {
+    id: 3,
+    name: "Astro",
+    image: "../../public/icons/astro-icon.png",
+  },
+};
+
+export default tags;
