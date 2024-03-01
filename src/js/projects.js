f46b484a60029ed2bbe09460f519d8450a0effe2
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2024-02-29 20:30:16 -0600

    feat: Add new projects in projects object array

--- src/js/projects.js
+++ src/js/projects.js
@@ -26,6 +26,66 @@ const projects = [
     githubUrl: "",
     tagIds: ["bubble"],
   },
+  {
+    id: 3,
+    title: "Spotify UI Clone",
+    description:
+      "The Spotify clone, non-functional, is just the user interface of the desktop application's home page. It is not responsive. The idea behind this project is to showcase my pixel-perfect skills.",
+    image: "../../public/img/spotify-ui-clone.png",
+    url: "https://spotifyuicln.netlify.app",
+    githubUrl: "https://github.com/JorgeSarricolea/Spotify-UI-Clone",
+    tagIds: ["html", "css", "javascript"],
+  },
+  {
+    id: 4,
+    title: "Joorge Search",
+    description:
+      "This is a small search engine with simulated Google-like recommendations, the intention is to demonstrate my UI skills.",
+    image: "../../public/img/joorge-search.png",
+    url: "https://joorge-search.netlify.app",
+    githubUrl: "https://github.com/JorgeSarricolea/Joorge-Search",
+    tagIds: ["html", "css", "javascript"],
+  },
+  {
+    id: 5,
+    title: "Weather App",
+    description:
+      "When working with REST APIs, the ideal project to practice is to make a weather app. I have used the free OpenWeatherMap API. The design is minimalist, and I have used the icons that this API contains.",
+    image: "../../public/img/weather-app.png",
+    url: "https://wtrapp.netlify.app",
+    githubUrl: "https://github.com/JorgeSarricolea/Weather-APP",
+    tagIds: ["html", "css", "javascript"],
+  },
+  {
+    id: 6,
+    title: "Rock, Paper or Scissors",
+    description:
+      "The famous Rock, Paper, Scissors game, play against the computer and win?... Designed to be responsive and simple.",
+    image: "../../public/img/rock-paper-scissors.png",
+    url: "https://roc-paper-scissors.netlify.app",
+    githubUrl: "https://github.com/JorgeSarricolea/Rock-Paper-Scissors-Game",
+    tagIds: ["html", "css", "javascript"],
+  },
+  {
+    id: 7,
+    title: "Recipies Net",
+    description:
+      "This is a recipe search engine obtained through the MealDB API. It features an attractive and responsive user interface that allows users to search for a wide variety of culinary recipes.",
+    image: "../../public/img/recipies-net.png",
+    url: "https://recipies-net.netlify.app",
+    githubUrl: "https://github.com/JorgeSarricolea/Recipies-NET",
+    tagIds: ["html", "css", "javascript"],
+  },
+  {
+    id: 8,
+    title: "Calorie Calculator",
+    description:
+      "This is a calorie intake calculator designed to help users determine their daily caloric needs based on their level of physical activity. It features a user-friendly and responsive design that makes it easy for individuals to calculate their ideal caloric intake.",
+    image: "../../public/img/calorie-calculator.png",
+    url: "https://kcal-calc.netlify.app",
+    githubUrl: "https://github.com/JorgeSarricolea/Calorie-Calculator",
+    tagIds: ["html", "css", "javascript"],
+  },
 ];
 
 export default projects;
