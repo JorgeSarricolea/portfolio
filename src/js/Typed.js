63461981729709a8283aa6ede497287e43be7ed9
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2024-03-05 11:53:16 -0600

    fix: TypedJS CDN

--- src/js/Typed.js
+++ src/js/Typed.js
@@ -1,11 +1,20 @@
-import Typed from "typed.js";
-
 document.addEventListener("DOMContentLoaded", function () {
-  let multiText = new Typed("#multiText", {
-    strings: ["a Fullstack Developer", "a UX/UI Designer", "Jorge Sarricolea"],
-    loop: true,
-    typeSpeed: 30,
-    backSpeed: 30,
-    backDelay: 2000,
-  });
+  import("typed.js")
+    .then((Typed) => {
+      // Una vez que la biblioteca se haya cargado correctamente, puedes crear la instancia de Typed
+      let multiText = new Typed.default("#multiText", {
+        strings: [
+          "a Fullstack Developer",
+          "a UX/UI Designer",
+          "Jorge Sarricolea",
+        ],
+        loop: true,
+        typeSpeed: 30,
+        backSpeed: 30,
+        backDelay: 2000,
+      });
+    })
+    .catch((error) => {
+      console.error('Error al cargar "typed.js"', error);
+    });
 });
