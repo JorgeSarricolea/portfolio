99e09be314b1dd0d6458ad029aa05dbb05c254c6
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2024-03-07 13:54:27 -0600

    feat: Create workshop page for personal use

---
+++ src/js/objects/workshopContent.js
@@ -0,0 +1,184 @@
+const workshopContent = {
+  "session-1": {
+    title: "Session 1. Introduction",
+    "topic-1": {
+      title: "Workshop main goal",
+      url: "",
+      check: "fa-solid fa-square-check",
+      notCheck: "fa-regular fa-square",
+      isCheck: false,
+    },
+    "topic-2": {
+      title: "Demonstration of the final project",
+      url: "",
+      check: "fa-solid fa-square-check",
+      notCheck: "fa-regular fa-square",
+      isCheck: false,
+    },
+    "topic-3": {
+      title: "Git installation (Ubuntu or Windows)",
+      url: "https://github.com/JorgeSarricolea/Learning-Git",
+      check: "fa-solid fa-square-check",
+      notCheck: "fa-regular fa-square",
+      isCheck: false,
+    },
+    "topic-4": {
+      title: "First steps, make the first commit",
+      url: "",
+      check: "fa-solid fa-square-check",
+      notCheck: "fa-regular fa-square",
+      isCheck: false,
+    },
+    "topic-5": {
+      title: "Create a GitHub account and upload a local repository",
+      url: "",
+      check: "fa-solid fa-square-check",
+      notCheck: "fa-regular fa-square",
+      isCheck: false,
+    },
+  },
+
+  "session-2": {
+    title: "Session 2. Software Engineering",
+    "topic-1": {
+      title: "How to start a web development project?",
+      url: "",
+      check: "fa-solid fa-square-check",
+      notCheck: "fa-regular fa-square",
+      isCheck: false,
+    },
+    "topic-2": {
+      title: "Branches: dev, prod, hot-fixes",
+      url: "",
+      check: "fa-solid fa-square-check",
+      notCheck: "fa-regular fa-square",
+      isCheck: false,
+    },
+    "topic-3": {
+      title: "What is Front-End?",
+      url: "",
+      check: "fa-solid fa-square-check",
+      notCheck: "fa-regular fa-square",
+      isCheck: false,
+    },
+    "topic-4": {
+      title: "What is Back-End?",
+      url: "",
+      check: "fa-solid fa-square-check",
+      notCheck: "fa-regular fa-square",
+      isCheck: false,
+    },
+    "topic-5": {
+      title: "Mock-ups, layout the form",
+      url: "",
+      check: "fa-solid fa-square-check",
+      notCheck: "fa-regular fa-square",
+      isCheck: false,
+    },
+    "topic-7": {
+      title: "Clone real project to local repository",
+      url: "https://github.com/JorgeSarricolea/FullStack-Workshop",
+      check: "fa-solid fa-square-check",
+      notCheck: "fa-regular fa-square",
+      isCheck: false,
+    },
+  },
+
+  "session-3": {
+    title: "Session 3. Stack",
+    "topic-1": {
+      title: "Organization for the development of a project",
+      url: "",
+      check: "fa-solid fa-square-check",
+      notCheck: "fa-regular fa-square",
+      isCheck: false,
+    },
+    "topic-2": {
+      title: "NodeJS installation",
+      url: "https://nodejs.org/en",
+      check: "fa-solid fa-square-check",
+      notCheck: "fa-regular fa-square",
+      isCheck: false,
+    },
+    "topic-3": {
+      title: "What is Astro? Astro installation",
+      url: "https://docs.astro.build/es/install/auto/",
+      check: "fa-solid fa-square-check",
+      notCheck: "fa-regular fa-square",
+      isCheck: false,
+    },
+    "topic-4": {
+      title: "TailwindCSS or CSS?",
+      url: "https://tailwindcss.com/docs/guides/astro",
+      check: "fa-solid fa-square-check",
+      notCheck: "fa-regular fa-square",
+      isCheck: false,
+    },
+    "topic-5": {
+      title: "Creation of the database",
+      url: "https://app.diagrams.net",
+      check: "fa-solid fa-square-check",
+      notCheck: "fa-regular fa-square",
+      isCheck: false,
+    },
+    "topic-6": {
+      title: "Deployment of the database in PlanetScale",
+      url: "https://auth.planetscale.com/sign-in",
+      check: "fa-solid fa-square-check",
+      notCheck: "fa-regular fa-square",
+      isCheck: false,
+    },
+  },
+
+  "session-4": {
+    title: "Session 4. Hard Coding",
+    "topic-1": {
+      title: "What is an API?",
+      url: "https://www.youtube.com/watch?v=9ZVooCNbdlY&t=2871s",
+      check: "fa-solid fa-square-check",
+      notCheck: "fa-regular fa-square",
+      isCheck: false,
+    },
+    "topic-2": {
+      title: "Front-End development",
+      url: "",
+      check: "fa-solid fa-square-check",
+      notCheck: "fa-regular fa-square",
+      isCheck: false,
+    },
+    "topic-3": {
+      title: "Back-End development",
+      url: "",
+      check: "fa-solid fa-square-check",
+      notCheck: "fa-regular fa-square",
+      isCheck: false,
+    },
+  },
+
+  "session-5": {
+    title: "Session 5. Testing and Deployment",
+    "topic-1": {
+      title: "Front-End deployment to prod",
+      url: "https://www.netlify.com",
+      check: "fa-solid fa-square-check",
+      notCheck: "fa-regular fa-square",
+      isCheck: false,
+    },
+    "topic-2": {
+      title: "Back-End deployment to prod",
+      url: "https://render.com",
+      check: "fa-solid fa-square-check",
+      notCheck: "fa-regular fa-square",
+      isCheck: false,
+    },
+    "topic-3": {
+      title: "Documentation, creation of README.md",
+      url: "https://readme.so",
+      check: "fa-solid fa-square-check",
+      notCheck: "fa-regular fa-square",
+      isCheck: false,
+    },
+  },
+};
+
+export default workshopContent;
