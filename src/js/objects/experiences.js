5f0e60ddb17b43a16f7344a12283f5d7c3ce71ec
Jorge Sarricolea <jjorgesarricolea18@email.com>
2024-05-17 14:44:53 -0600

    feat (data): add new experiences
        Added new experiences to the experiences object in experiences.js.

--- src/js/objects/experiences.js
+++ src/js/objects/experiences.js
@@ -1,21 +1,51 @@
 const experiences = [
   {
     id: 1,
-    company: "Datamatic Software",
-    position: "Web Developer",
-    date: "2022 -2022",
+    company: "Sweet fit",
+    position: "Full-Stack Developer",
+    date: "April 2024 - May 2024",
     description:
-      "I worked on different projects, designing the user interface from scratch, adding new features and reusable components for various projects.",
+      "I developed a custom administrative platform from scratch for Sweet Fit restaurant, facilitating coupon management and allocation to attract new customers. I led back-end development in JavaScript and collaborated with a team for the front-end development.",
+    image: "../img/SweetFit.png",
+    url: "https://sweetandfit.mx/",
+  },
+  {
+    id: 2,
+    company: "Seguros Gutierrez y Arredondo",
+    position: "Django Developer",
+    date: "April 2024 - May 2024",
+    description:
+      "I developed a data retrieval form using Django and PostgreSQL to streamline the prospect acquisition process and enhance efficiency for the recruitment team. Focus was on seamlessly capturing and storing prospect data to optimize lead management.",
+    image: "../img/GrupoGyA.png",
+    url: "https://seragentedeseguros.mx/gya",
+  },
+  {
+    id: 3,
+    company: "ICCBR 2024",
+    position: "Full-Stack Developer",
+    date: "January 2024 - March 2024",
+    description:
+      "I collaborated in a team to refactor the entire platform, transitioning from PHP to Astro + JavaScript to remove vulnerabilities. My primary focus included building a robust RESTful API for payment processing and email notifications for ticket purchases. Additionally, I contributed to frontend improvements by refactoring and creating UI components.",
+    image: "../img/ICCBR2024.png",
+    url: "https://iccbr2024.org/",
+  },
+  {
+    id: 4,
+    company: "Datamatic Software",
+    position: "Bubble Developer",
+    date: "June 2022 - July 2022",
+    description:
+      "I was tasked with specific responsibilities across various projects. This included bug fixing, enhancing user interfaces, improving back-end workflows, and updating APIs consumed by each project.",
     image: "../img/DatamaticSoftware.jpg",
     url: "https://www.datamaticsoftware.com",
   },
   {
-    id: 2,
+    id: 5,
     company: "Jumwork",
-    position: "Web Developer",
-    date: "2018 -2022",
+    position: "Bubble Developer",
+    date: "June 2018 - Janurary 2022",
     description:
-      "I designed the whole brand, UX/UI design from scratch on Bubble. Integration with Stripe API and others in backend process. Worked with different plugins, like Sendgrid and upgraded old pages to the new responsive engine.",
+      "I worked on developing the platform from scratch. This platform is a marketplace with an integrated chat, auction system, email notifications, and a payment gateway via Stripe. My primary goal was to work on all modules to deliver a functional, responsive platform without any development errors.",
     image: "../img/JumworkTareas.png",
     url: "https://www.jumwork.com",
   },
