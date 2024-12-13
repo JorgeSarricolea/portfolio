85fa8e07b0e13e201b555dc611f708b298f48389
Jorge Sarricolea <jjorgesarricolea18@email.com>
2024-12-13 00:35:36 -0600

    feat(cv): update label, summary, and graduation date
        - Updated the professional label to reflect expertise and specialization:
            - "Innovative Software Engineer | Expert in JavaScript & API Development | React, Node.js, Java Spring Boot & AWS Specialist | 3+ Years of Experience in Scalable Web Applications | Team Mentorship".
        - Revised the summary to emphasize skills, experience, and career goals.
        - Adjusted the graduation date of the education section to '2025' for accuracy.

--- src/js/objects/cv.js
+++ src/js/objects/cv.js
@@ -2,11 +2,11 @@ const cv = {
   basics: {
     name: "Jorge Sarricolea Veyro",
     label:
-      "Full-Stack developer with 3 years of hands-on experience, deeply passionate about technology and constantly learning with the latest innovations.",
+      "Innovative Software Engineer | Expert in JavaScript & API Development | React, Node.js, Java Spring Boot & AWS Specialist | 3+ Years of Experience in Scalable Web Applications | Team Mentorship",
     email: "jjorgesarricolea@gmail.com",
     phone: "+52 9381095593",
     summary:
-      "3 years ago, I embarked on my journey in web development, creating attractive and functional web applications. My focus is on merging technical skills with a keen eye for design, which drives me to constantly advance. I firmly believe that true changes requires order, discipline, and perseverance, which are essential for achieving success.",
+      "Software Engineer with 3 years of experience specializing in Angular, React, Java Spring Boot, Node.js, and AWS. I develop scalable, high-performance web applications that combine solid back-end architecture with intuitive front-end design.\n\n Passionate about staying up to date with the latest technologies, I focus on optimizing workflows, solving complex problems, and mentoring junior developers to deliver impactful solutions. My goal is to continue expanding my technical expertise while contributing to innovative and value-driven projects.",
   },
   social: {
     portfolio: "https://jorgesarricolea.com",
@@ -15,7 +15,7 @@ const cv = {
   },
   education: {
     institution: "Instituto Tecnológico de Mérida",
-    date: "2020 - 2024",
+    date: "2020 - 2025",
     career: "Computer Stystems Engineer",
   },
 };
