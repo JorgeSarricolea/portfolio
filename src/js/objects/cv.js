9429e120b9c3c82d2404d7bcdcf4f8e231d9568e
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2025-02-25 10:55:11 -0600

    chore(cv): update contact information in cv.js
        - Updated email to jjorgesarricolea18@gmail.com.
        - Updated phone number format for better readability.

--- src/js/objects/cv.js
+++ src/js/objects/cv.js
@@ -3,8 +3,8 @@ const cv = {
     name: "Jorge Sarricolea Veyro",
     label:
       "Software Engineer | Expert in JavaScript, Node.js, and API Development | 4 Years of Experience in Scalable Backend Systems | Focused on RESTful APIs, Express.js, and Performance Optimization",
-    email: "jjorgesarricolea@gmail.com",
-    phone: "+52 9381095593",
+    email: "jjorgesarricolea18@gmail.com",
+    phone: "+52 938 109 5593",
     summary:
       "Software Engineer with 4 years of experience specializing in JavaScript, Node.js, and API development. I focus on building scalable and high-performance backend systems, with expertise in RESTful APIs, Express.js, and performance optimization.\n\nIn addition to my backend expertise, I also have experience in frontend development, working with frameworks like React and Angular to deliver seamless and intuitive user interfaces. My goal is to leverage my skills to create efficient, scalable solutions that drive impactful results.",
   },
