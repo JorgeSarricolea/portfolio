fe1c6768acb0ca4d9e77a15848c1ab252009e553
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2025-04-30 13:20:51 -0600

    refactor(tags): reorganize tech stack into categorized groups
        - Grouped tags under categories such as 'languages', 'frontend', 'backend', 'devops', and more.
        - Updated certain tag names for clarity (e.g., 'JavaScript' to 'JavaScript (ES6+)').
        - Added new technologies like 'Vite', 'Supertest', and 'Oracle' with appropriate priorities.
        - Improves maintainability and enables scalable skill categorization.

--- src/js/objects/tags.js
+++ src/js/objects/tags.js
@@ -1,160 +1,159 @@
 const tags = {
-  // Main
-  javascript: {
-    name: "JavaScript",
-    image: "../icons/javascript-icon.png",
-    priority: "main",
+  languages: {
+    javascript: {
+      name: "JavaScript (ES6+)",
+      image: "../icons/javascript-icon.png",
+      priority: "main",
+    },
+    typescript: {
+      name: "TypeScript",
+      image: "../icons/typescript-icon.png",
+      priority: "main",
+    },
+    java: {
+      name: "Java",
+      image: "../icons/java-icon.png",
+      priority: "default",
+    },
+    python: {
+      name: "Python",
+      image: "../icons/python-icon.png",
+      priority: "default",
+    },
+    php: {
+      name: "PHP",
+      image: "../icons/php-icon.png",
+      priority: "default",
+    },
   },
-  typescript: {
-    name: "TypeScript",
-    image: "../icons/typescript-icon.png",
-    priority: "main",
+  devops: {
+    aws: {
+      name: "AWS",
+      image: "../icons/aws-icon.png",
+      priority: "main",
+    },
+    docker: {
+      name: "Docker",
+      image: "../icons/docker-icon.png",
+      priority: "main",
+    },
+    git: {
+      name: "Git",
+      image: "../icons/git-icon.png",
+      priority: "default",
+    },
   },
-  nodejs: {
-    name: "Node.js",
-    image: "../icons/nodejs-icon.png",
-    priority: "main",
+  backend: {
+    nodejs: {
+      name: "Node.js",
+      image: "../icons/nodejs-icon.png",
+      priority: "main",
+    },
+    expressjs: {
+      name: "Express.js",
+      image: "../icons/express-icon.png",
+      priority: "main",
+    },
+    springboot: {
+      name: "Spring Boot",
+      image: "../icons/springboot-icon.png",
+      priority: "default",
+    },
+    django: {
+      name: "Django",
+      image: "../icons/django-icon.png",
+      priority: "default",
+    },
+    prisma: {
+      name: "Prisma",
+      image: "../icons/prisma-icon.png",
+      priority: "main",
+    },
   },
-  expressjs: {
-    name: "Express.js",
-    image: "../icons/express-icon.png",
-    priority: "main",
+  frontend: {
+    reactjs: {
+      name: "React.js",
+      image: "../icons/reactjs-icon.png",
+      priority: "main",
+    },
+    nextjs: {
+      name: "Next.js",
+      image: "../icons/nextjs-icon.png",
+      priority: "main",
+    },
+    angular: {
+      name: "Angular",
+      image: "../icons/angular-icon.png",
+      priority: "default",
+    },
+    astro: {
+      name: "Astro",
+      image: "../icons/astro-icon.png",
+      priority: "main",
+    },
+    tailwindcss: {
+      name: "TailwindCSS",
+      image: "../icons/tailwindcss-icon.png",
+      priority: "main",
+    },
+    vite: {
+      name: "Vite",
+      image: "../icons/vite-icon.png",
+      priority: "main",
+    },
   },
-  aws: {
-    name: "AWS",
-    image: "../icons/aws-icon.png",
-    priority: "main",
+  databases: {
+    postgresql: {
+      name: "PostgreSQL",
+      image: "../icons/postgresql-icon.png",
+      priority: "default",
+    },
+    mysql: {
+      name: "MySQL",
+      image: "../icons/mysql-icon.png",
+      priority: "default",
+    },
+    oracle: {
+      name: "Oracle",
+      image: "../icons/oracle-icon.png",
+      priority: "default",
+    },
   },
-  docker: {
-    name: "Docker",
-    image: "../icons/docker-icon.png",
-    priority: "main",
+  testing: {
+    jest: {
+      name: "Jest",
+      image: "../icons/jest-icon.png",
+      priority: "main",
+    },
+    supertest: {
+      name: "Supertest",
+      image: "../icons/supertest-icon.png",
+      priority: "main",
+    },
   },
-  jest: {
-    name: "Jest",
-    image: "../icons/jest-icon.png",
-    priority: "main",
+  blockchain: {
+    wagmi: {
+      name: "Wagmi",
+      image: "../icons/wagmi-icon.png",
+      priority: "default",
+    },
+    scaffold: {
+      name: "Scaffold",
+      image: "../icons/scaffold-icon.png",
+      priority: "default",
+    },
   },
-  prisma: {
-    name: "Prisma",
-    image: "../icons/prisma-icon.png",
-    priority: "main",
-  },
-  nextjs: {
-    name: "Next.js",
-    image: "../icons/nextjs-icon.png",
-    priority: "main",
-  },
-  reactjs: {
-    name: "React.js",
-    image: "../icons/reactjs-icon.png",
-    priority: "main",
-  },
-  swagger: {
-    name: "Swagger",
-    image: "../icons/swagger-icon.png",
-    priority: "main",
-  },
-  astro: {
-    name: "Astro",
-    image: "../icons/astro-icon.png",
-    priority: "main",
-  },
-  tailwindcss: {
-    name: "TailwindCSS",
-    image: "../icons/tailwindcss-icon.png",
-    priority: "main",
-  },
-  // Default
-  angular: {
-    name: "Angular",
-    image: "../icons/angular-icon.png",
-    priority: "default",
-  },
-  springboot: {
-    name: "SpringBoot",
-    image: "../icons/springboot-icon.png",
-    priority: "default",
-  },
-  maven: {
-    name: "Maven",
-    image: "../icons/maven-icon.png",
-    priority: "default",
-  },
-  scaffold: {
-    name: "Scaffold",
-    image: "../icons/scaffold-icon.png",
-    priority: "default",
-  },
-  wagmi: {
-    name: "Wagmi",
-    image: "../icons/wagmi-icon.png",
-    priority: "default",
-  },
-  threejs: {
-    name: "Three.js",
-    image: "../icons/threejs-icon.png",
-    priority: "default",
-  },
-  django: {
-    name: "Django",
-    image: "../icons/django-icon.png",
-    priority: "default",
-  },
-  postgresql: {
-    name: "PostgreSQL",
-    image: "../icons/postgresql-icon.png",
-    priority: "default",
-  },
-  mysql: {
-    name: "MySQL",
-    image: "../icons/mysql-icon.png",
-    priority: "default",
-  },
-  html: {
-    name: "HTML",
-    image: "../icons/html-icon.png",
-    priority: "default",
-  },
-  css: {
-    name: "CSS",
-    image: "../icons/css-icon.png",
-    priority: "default",
-  },
-  bootstrap: {
-    name: "Bootstrap",
-    image: "../icons/bootstrap-icon.png",
-    priority: "default",
-  },
-  git: {
-    name: "Git",
-    image: "../icons/git-icon.png",
-    priority: "default",
-  },
-  php: {
-    name: "PHP",
-    image: "../icons/php-icon.png",
-    priority: "default",
-  },
-  java: {
-    name: "Java",
-    image: "../icons/java-icon.png",
-    priority: "default",
-  },
-  python: {
-    name: "Python",
-    image: "../icons/python-icon.png",
-    priority: "default",
-  },
-  jquery: {
-    name: "jQuery",
-    image: "../icons/jquery-icon.png",
-    priority: "default",
-  },
-  bubble: {
-    name: "Bubble",
-    image: "../icons/bubbleio-icon.png",
-    priority: "default",
+  others: {
+    swagger: {
+      name: "Swagger",
+      image: "../icons/swagger-icon.png",
+      priority: "main",
+    },
+    bubble: {
+      name: "Bubble.io",
+      image: "../icons/bubbleio-icon.png",
+      priority: "default",
+    },
   },
 };
 
